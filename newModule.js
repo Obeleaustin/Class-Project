6f17687d5bc5d0a6685f2d7cdf54ddad4edbc5cd
Obele Austin <obeleoyeinfie@gmail.com>
2023-09-19 06:52:08 +0100

    all commit

---
+++ newModule.js
@@ -0,0 +1,84 @@
+export let courseData = [
+    {
+        courseTitle : 'Desktop Publishing',
+        price : 75000,
+        courseDescription : 'Being fully literate about your Computer.',
+        location: 'Yenagoa'
+
+    },
+    {
+        courseTitle : 'Electronics',
+        price : 50000,
+        courseDescription : 'Learn all about hardwares.',
+        location: 'Yenagoa'
+
+    },
+    {
+        courseTitle : 'Web Development',
+        price : 100000,
+        courseDescription : 'Get to know how to build your own website',
+        location: 'Yenagoa',
+        frameWork: 'React'
+
+    },
+    {
+        courseTitle : 'Computer Hardware Repairs and Maintenance',
+        price : 50000,
+        courseDescription : 'Get your computers cared for.',
+        location: 'Yenagoa'
+
+    },
+    {
+        courseTitle : 'Kids Code',
+        price : 2000,
+        courseDescription : 'Show your kids the world of technology today.',
+        location: 'Yenagoa'
+
+    },
+    {
+        courseTitle : 'Graphics Design',
+        price : 8000,
+        courseDescription : 'Be your own master artist and designer.',
+        location: 'Yenagoa'
+
+    },
+    {
+        courseTitle : 'Digital Marketing',
+        price : 40000,
+        courseDescription : 'Learn how to scale the digital market.',
+        location: 'Yenagoa'
+
+    }
+]
+ 
+
+// allCourses.push(desktopPublishing.detalis(), Electronics.detalis(),)
+// console.log(allCourses[0])
+
+
+// for(j = 0; j < array.length - 1; j ++){
+//     console.log(array[j] + 10000)
+//     increasedPrice.push(array[j] + 5000)
+
+// }
+// checkInput.forEach((item)=>{
+//     allInput.push(item);
+// })
+
+// allInput.forEach((input)=>{
+//     input.addEventListener('click',()=>{
+//         if(inputYes.checked && InputOnline.checked){
+//         console.log('me');
+            
+//             display.innerHTML = desktopPublishing.detalis();
+//         }
+//     })
+// })
+//  desktopPublishing = new courseDetails('Desktop Publishing', increasedPrice[0], 'the best', 'Yenagoa')
+
+
+//  // console.log(array1)
+// // console.log(price)
+// console.log(desktopPublishing.detalis())
+// console.log(Electronics.detalis())
+
